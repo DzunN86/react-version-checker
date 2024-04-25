5d191e28875c342755e33959e66e383e7cd5eea2
Dzun <nurroinz@gmail.com>
2024-04-25 16:16:26 +0700

    handle script with node

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { execSync } from "child_process";
+import fs from 'fs';
+import path from 'path';
 
-// Get current tag/commit and last commit date from git
-const version = execSync("./scripts/get-version.sh").toString().trim();
+// get version from package.json and git
+const packageJson = JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf8'));
+const version = packageJson.version;
+const gitCommitCount = execSync('git rev-list --count HEAD').toString().trim();
+const gitCommitHash = execSync('git rev-parse --short HEAD').toString().trim();
+const appVersion = `${version}-${gitCommitCount}-${gitCommitHash}`;
+
+// sync version to public folder
+const signature = {version: appVersion};
+fs.writeFileSync(path.join(__dirname, './public/signature.json'), JSON.stringify(signature, null, 2));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    __APP_VERSION__: JSON.stringify(version),
+    __APP_VERSION__: JSON.stringify(appVersion),
     __VERSION_CHECK_INTERVAL__: 1000 * 30, // check new version every 30 seconds
   },
 })
